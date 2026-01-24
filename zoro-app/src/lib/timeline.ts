86b7c2f1657510b171332ec11b8a66e205f0f99f
Mazin <mazin.biviji1@gmail.com>
2026-01-24 16:28:26 +0700

    More minor text edits to timeline

--- zoro-app/src/lib/timeline.ts
+++ zoro-app/src/lib/timeline.ts
@@ -16,25 +16,25 @@ export const TIMELINE_MILESTONES: TimelineMilestone[] = [
     count: 10,
     displayCount: '10',
     title: 'First 10',
-    description: '30-minute free portfolio review and planning meeting directly with our founder.',
+    description: '30-minute free portfolio review and planning meeting directly with our founding team',
   },
   {
     count: 100,
     displayCount: '100',
     title: 'First 100',
-    description: 'Receive human reviewed email and early access to new features for free for 1 year',
+    description: 'Receive human reviewed email free for 1 year and early access to new features',
   },
   {
     count: 1000,
     displayCount: '1k',
     title: 'First 1,000',
-    description: 'We take the RIA exam and offer comprehensive planning free for 2 years',
+    description: 'We take the RIA exam and offer AI powered comprehensive planning free for 1 year',
   },
   {
     count: 10000,
     displayCount: '10k',
     title: 'First 10,000',
-    description: 'Launch as a paid app in the app and play stores but free for life for early adopter',
+    description: 'Launch as a paid app in the app and play stores, free only for life for early adopters',
   },
 ];
 
